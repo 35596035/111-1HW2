79e1abeb6ad94001497d641a29086ae0961c5790
35596035 <william82733791@gmail.com>
2022-09-28 14:59:17 +0800

    Finial

--- 111-1HW2/Bomd.aspx.cs
+++ 111-1HW2/Bomd.aspx.cs
@@ -12,37 +12,91 @@ namespace _111_1HW2
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            int[] ia_Mlndex = new int[0, 7, 13, 28, 44, 62, 74, 75, 87, 90];
+            int[] ia_Mlndex = new int[10] { 0, 7, 13, 28, 44, 62, 74, 75, 87, 90 };
             char[,] ia_Map = new char[10, 10];
-            for (int i_Row = 0 ; i_Row < 10 ; i_Row++)
+            for (int i_Row = 0; i_Row < 10; i_Row++)
             {
-                for (int i_Col = 0; i_Cok < 10; i_Col++)
+                for (int i_Col = 0; i_Col < 10; i_Col++)
                 {
-                    ia_Map[i_Row, i_Col] = "O";
+                    ia_Map[i_Row, i_Col] = 'O';
                 }
             }
-
+            #region 塞炸彈位置和判斷炸彈周圍數字顯示
             for (int i_Ct = 0; i_Ct < 10; i_Ct++)
             {
-                int i_Row = ia_Mlndex[i_Ct]/10;
+                int i_Row = ia_Mlndex[i_Ct] / 10;
                 int i_Col = ia_Mlndex[i_Ct] % 10;
-                ia_Map[i_Row, i_Ct] = "*";
+                ia_Map[i_Row, i_Col] = '*';
+                if (ia_Map[i_Row, i_Col] == '*')
+                {
+                    continue;
+                }
+                //左上
+                if ((i_Row - 1) >= 0 && (i_Col - 1) >= 0)
+                {
+                    int i_Num = Convert.ToInt32(ia_Map[i_Row, i_Col]);
+                    i_Num++;
+                    ia_Map[i_Row, i_Col] = Convert.ToChar(i_Num);
+                }
+                //上
+                if ((i_Row - 1) >= 0 && (i_Col - 0) >= 0)
+                {
+                    int i_Num = Convert.ToInt32(ia_Map[i_Row, i_Col]);
+                    i_Num++;
+                    ia_Map[i_Row, i_Col] = Convert.ToChar(i_Num);
+                }
+                //右上
+                if ((i_Row - 1) >= 0 && (i_Col + 1) >= 0)
+                {
+                    int i_Num = Convert.ToInt32(ia_Map[i_Row, i_Col]);
+                    i_Num++;
+                    ia_Map[i_Row, i_Col] = Convert.ToChar(i_Num);
+                }
+                //左
+                if ((i_Row - 0) >= 0 && (i_Col - 1) >= 0)
+                {
+                    int i_Num = Convert.ToInt32(ia_Map[i_Row, i_Col]);
+                    i_Num++;
+                    ia_Map[i_Row, i_Col] = Convert.ToChar(i_Num);
+                }
+                //右
+                if ((i_Row - 0) >= 0 && (i_Col + 1) >= 0)
+                {
+                    int i_Num = Convert.ToInt32(ia_Map[i_Row, i_Col]);
+                    i_Num++;
+                    ia_Map[i_Row, i_Col] = Convert.ToChar(i_Num);
+                }
+                //左下
+                if ((i_Row + 1) >= 0 && (i_Col - 1) >= 0)
+                {
+                    int i_Num = Convert.ToInt32(ia_Map[i_Row, i_Col]);
+                    i_Num++;
+                    ia_Map[i_Row, i_Col] = Convert.ToChar(i_Num);
+                }
+                //下
+                if ((i_Row + 1) >= 0 && (i_Col - 0) >= 0)
+                {
+                    int i_Num = Convert.ToInt32(ia_Map[i_Row, i_Col]);
+                    i_Num++;
+                    ia_Map[i_Row, i_Col] = Convert.ToChar(i_Num);
+                }
+                //右下
+                if ((i_Row + 1) >= 0 && (i_Col + 1) >= 0)
+                {
+                    int i_Num = Convert.ToInt32(ia_Map[i_Row, i_Col]);
+                    i_Num++;
+                    ia_Map[i_Row, i_Col] = Convert.ToChar(i_Num);
+                }
             }
+            #endregion
 
             for (int i_Row = 0; i_Row < 10; i_Row++)
             {
                 for (int i_Col = 0; i_Col < 10; i_Col++)
                 {
-                    if (ia_Map[i_Row, i_Col] == "*")
-                    {
-                        continue;
-                    }
-
-                    if ((i_Row -1) >= 0 && (i_Col - 1) > = 0)
-                    {
-                        int i_Num = Convert.ToInt32(ia_Map[i_Row, i_Col]);                           
-                    }
+                    Response.Write(ia_Map[i_Row, i_Col]);
                 }
+                Response.Write("<br />");
             }
         }
     }
