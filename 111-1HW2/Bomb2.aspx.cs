83ac105c2022229f13260df7685cd93b1a3c8770
35596035 <william82733791@gmail.com>
2022-09-28 18:32:34 +0800

    Finial_for

---
+++ 111-1HW2/Bomb2.aspx.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _111_1HW2
+{
+    public partial class Bomb2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //if寫法
+            int[] ia_Mlndex = new int[10] { 0, 7, 13, 28, 44, 62, 74, 75, 87, 90 };
+            char[,] ia_Map = new char[10, 10];
+            for (int i_Row = 0; i_Row< 10; i_Row++)
+            {
+                for (int i_Col = 0; i_Col< 10; i_Col++)
+                {
+                    ia_Map[i_Row, i_Col] = '0';
+                }
+            }
+
+            for (int i_Ct = 0; i_Ct < 10; i_Ct++)
+            {
+                int i_Row = ia_Mlndex[i_Ct] / 10; //0
+                int i_Col = ia_Mlndex[i_Ct] % 10; //0
+                ia_Map[i_Row, i_Col] = '*';
+            }
+            for (int i_Row = 0; i_Row < 10; i_Row++)
+            {
+                int BombCt = 0;
+                for (int i_Col = 0; i_Col < 10; i_Col++)
+                {
+                    if (ia_Map[i_Row, i_Col] == '*')
+                    {
+                        continue;
+                    }
+                    Response.Write(i_Row);
+                    Response.Write(i_Col);
+                    Response.Write("<br>");
+                    //左上
+                    if ((i_Row - 1) >= 0 && (i_Col - 1) >= 0)
+                    {
+                        //左上
+                        if (ia_Map[i_Row - 1, i_Col - 1] == '*')
+                        {
+                            BombCt++;
+                            break;
+                        }
+                        /*
+                        //上
+                        if (ia_Map[i_Row - 1, i_Col - 0] == '*')
+                        {
+                            BombCt++;
+                        }
+                        //右上
+                        if (ia_Map[i_Row - 1, i_Col + 1] == '*')
+                        {
+                            BombCt++;
+                        }
+                        //左
+                        if (ia_Map[i_Row - 0, i_Col - 1] == '*')
+                        {
+                            BombCt++;
+                        }
+                        //右
+                        if (ia_Map[i_Row - 0, i_Col + 1] == '*')
+                        {
+                            BombCt++;
+                        }
+                        //左下
+                        if (ia_Map[i_Row + 1, i_Col - 1] == '*')
+                        {
+                            BombCt++;
+                        }
+                        //下
+                        if (ia_Map[i_Row + 1, i_Col - 0] == '*')
+                        {
+                            BombCt++;
+                        }
+                        //右下
+                        if (ia_Map[i_Row + 1, i_Col + 1] == '*')
+                        {
+                            BombCt++;
+                        }*/
+                    }
+                    //上
+                    if ((i_Row - 1) >= 0)
+                    {
+                        if (ia_Map[i_Row - 1, i_Col] == '*')
+                        {
+                            BombCt++;
+                        }
+                    }
+                    //右上
+                    if ((i_Row - 1) >= 0 && (i_Col + 1) < 10)
+                    {
+                        if (ia_Map[i_Row - 1, i_Col + 1] == '*')
+                        {
+                            BombCt++;
+                        }
+                    }
+                    //左
+                    if ((i_Col - 1) > 0)
+                    {
+                        if (ia_Map[i_Row, i_Col - 1] == '*')
+                        {
+                            BombCt++;
+                        }
+                    }
+
+                    //右
+                    if ((i_Col + 1) < 10) //Row:0-0=0 Col:0+1=1
+                    {
+                        if (ia_Map[i_Row, i_Col + 1] == '*')
+                        {
+                            BombCt++;
+                        }
+                    }
+
+                    //左下
+                    if ((i_Row + 1) < 10 && (i_Col - 1) >= 0)
+                    {
+                        if (ia_Map[i_Row + 1, i_Col - 1] == '*')
+                        {
+                            BombCt++;
+                        }
+                    }
+                    //下
+                    if ((i_Row + 1) < 10)
+                    {
+                        if (ia_Map[i_Row + 1, i_Col] == '*')
+                        {
+                            BombCt++;
+                        }
+                    }
+                    //右下
+                    if ((i_Row + 1) < 10 && (i_Col + 1) <= 9)
+                    {
+                        if (ia_Map[i_Row + 1, i_Col + 1] == '*')
+                        {
+                            BombCt++;
+                        }
+                    }
+                    Response.Write(Convert.ToInt32(BombCt) + "<br />");
+                    ia_Map[i_Row, i_Col] = Convert.ToChar(BombCt);
+                }
+                //#endregion
+            }
+
+            //methonds 2
+
+
+            for (int i_Row = 0; i_Row < 10; i_Row++)
+            {
+                for (int i_Col = 0; i_Col < 10; i_Col++)
+                {
+                    Response.Write(ia_Map[i_Row, i_Col]);
+                }
+                Response.Write("<br />");
+            }
+        }
+    }
+}
--- 111-1HW2/Bomd.aspx.cs
+++ 111-1HW2/Bomd.aspx.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using System.Runtime.dll;
 
 namespace _111_1HW2
 {
@@ -12,84 +11,122 @@ namespace _111_1HW2
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //第二種寫法for
+            //填入0
             int[] ia_Mlndex = new int[10] { 0, 7, 13, 28, 44, 62, 74, 75, 87, 90 };
             char[,] ia_Map = new char[10, 10];
             for (int i_Row = 0; i_Row < 10; i_Row++)
             {
                 for (int i_Col = 0; i_Col < 10; i_Col++)
                 {
-                    ia_Map[i_Row, i_Col] = 'O';
+                    ia_Map[i_Row, i_Col] = '0';
                 }
             }
-            #region 塞炸彈位置和判斷炸彈周圍數字顯示
+            //填入*
             for (int i_Ct = 0; i_Ct < 10; i_Ct++)
             {
-                int i_Row = ia_Mlndex[i_Ct] / 10;
-                int i_Col = ia_Mlndex[i_Ct] % 10;
+                int i_Row = ia_Mlndex[i_Ct] / 10; //0
+                int i_Col = ia_Mlndex[i_Ct] % 10; //0
                 ia_Map[i_Row, i_Col] = '*';
-                if (ia_Map[i_Row, i_Col] == '*')
-                {
-                    continue;
-                }
+                //Response.Write(ia_Map[i_Row, i_Col]);
+            }
+            //訪問*周邊
+            for (int i_Ct = 0; i_Ct < 10; i_Ct++)
+            {
+                int i_Row = ia_Mlndex[i_Ct] / 10; //0
+                int i_Col = ia_Mlndex[i_Ct] % 10; //0
                 //左上
                 if ((i_Row - 1) >= 0 && (i_Col - 1) >= 0)
                 {
-                    int i_Num = Convert.ToInt32(ia_Map[i_Row, i_Col]);
+                    if (ia_Map[i_Row - 1, i_Col - 1] == '*')
+                    {
+                        continue;
+                    }
+                    int i_Num = Convert.ToInt32(ia_Map[i_Row - 1, i_Col - 1]);
                     i_Num++;
-                    ia_Map[i_Row, i_Col] = Convert.ToChar(i_Num);
+                    ia_Map[i_Row - 1, i_Col - 1] = Convert.ToChar(i_Num);
                 }
                 //上
-                if ((i_Row - 1) >= 0 && (i_Col - 0) >= 0)
+                if ((i_Row - 1) >= 0)
                 {
-                    int i_Num = Convert.ToInt32(ia_Map[i_Row, i_Col]);
+                    if (ia_Map[i_Row - 1, i_Col] == '*')
+                    {
+                        continue;
+                    }
+                    int i_Num = Convert.ToInt32(ia_Map[i_Row - 1, i_Col]);
                     i_Num++;
-                    ia_Map[i_Row, i_Col] = Convert.ToChar(i_Num);
+                    ia_Map[i_Row - 1, i_Col] = Convert.ToChar(i_Num);
                 }
                 //右上
-                if ((i_Row - 1) >= 0 && (i_Col + 1) >= 0)
+                if ((i_Row - 1) >= 0 && (i_Col + 1) < 10)
                 {
-                    int i_Num = Convert.ToInt32(ia_Map[i_Row, i_Col]);
+                    if (ia_Map[i_Row - 1, i_Col + 1] == '*')
+                    {
+                        continue;
+                    }
+                    int i_Num = Convert.ToInt32(ia_Map[i_Row - 1, i_Col + 1]);
                     i_Num++;
-                    ia_Map[i_Row, i_Col] = Convert.ToChar(i_Num);
+                    ia_Map[i_Row - 1, i_Col + 1] = Convert.ToChar(i_Num);
                 }
                 //左
-                if ((i_Row - 0) >= 0 && (i_Col - 1) >= 0)
+                if ((i_Col - 1) >= 0)
                 {
-                    int i_Num = Convert.ToInt32(ia_Map[i_Row, i_Col]);
+                    if (ia_Map[i_Row, i_Col - 1] == '*')
+                    {
+                        continue;
+                    }
+                    int i_Num = Convert.ToInt32(ia_Map[i_Row, i_Col - 1]);
                     i_Num++;
-                    ia_Map[i_Row, i_Col] = Convert.ToChar(i_Num);
+                    ia_Map[i_Row, i_Col - 1] = Convert.ToChar(i_Num);
                 }
+
                 //右
-                if ((i_Row - 0) >= 0 && (i_Col + 1) >= 0)
+                if ((i_Col + 1) < 10)
                 {
-                    int i_Num = Convert.ToInt32(ia_Map[i_Row, i_Col]);
+                    if (ia_Map[i_Row, i_Col + 1] == '*')
+                    {
+                        continue;
+                    }
+                    int i_Num = Convert.ToInt32(ia_Map[i_Row, i_Col + 1]);
                     i_Num++;
-                    ia_Map[i_Row, i_Col] = Convert.ToChar(i_Num);
+                    ia_Map[i_Row, i_Col + 1] = Convert.ToChar(i_Num);
                 }
+
                 //左下
                 if ((i_Row + 1) >= 0 && (i_Col - 1) >= 0)
                 {
-                    int i_Num = Convert.ToInt32(ia_Map[i_Row, i_Col]);
+                    if (ia_Map[i_Row + 1, i_Col - 1] == '*')
+                    {
+                        continue;
+                    }
+                    int i_Num = Convert.ToInt32(ia_Map[i_Row + 1, i_Col - 1]);
                     i_Num++;
-                    ia_Map[i_Row, i_Col] = Convert.ToChar(i_Num);
+                    ia_Map[i_Row + 1, i_Col - 1] = Convert.ToChar(i_Num);
                 }
                 //下
-                if ((i_Row + 1) >= 0 && (i_Col - 0) >= 0)
+                if ((i_Row + 1) < 10)
                 {
-                    int i_Num = Convert.ToInt32(ia_Map[i_Row, i_Col]);
+                    if (ia_Map[i_Row + 1, i_Col] == '*')
+                    {
+                        continue;
+                    }
+                    int i_Num = Convert.ToInt32(ia_Map[i_Row + 1, i_Col]);
                     i_Num++;
-                    ia_Map[i_Row, i_Col] = Convert.ToChar(i_Num);
+                    ia_Map[i_Row + 1, i_Col] = Convert.ToChar(i_Num);
                 }
                 //右下
-                if ((i_Row + 1) >= 0 && (i_Col + 1) >= 0)
+                if ((i_Row + 1) < 10 && (i_Col + 1) < 10)
                 {
-                    int i_Num = Convert.ToInt32(ia_Map[i_Row, i_Col]);
+                    if (ia_Map[i_Row + 1, i_Col + 1] == '*')
+                    {
+                        continue;
+                    }
+                    int i_Num = Convert.ToInt32(ia_Map[i_Row + 1, i_Col + 1]);
                     i_Num++;
-                    ia_Map[i_Row, i_Col] = Convert.ToChar(i_Num);
+                    ia_Map[i_Row + 1, i_Col + 1] = Convert.ToChar(i_Num);
                 }
             }
-            #endregion
-
+            //最後訪問全部的陣列
             for (int i_Row = 0; i_Row < 10; i_Row++)
             {
                 for (int i_Col = 0; i_Col < 10; i_Col++)
@@ -98,6 +135,8 @@ namespace _111_1HW2
                 }
                 Response.Write("<br />");
             }
+            //Response.Write(ia_Map[7, 4]);
+            //Response.Write(ia_Map[7, 5]);
         }
     }
 }
